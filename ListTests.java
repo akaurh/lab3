fbbabb1c4e11a560f0c3f9f2ec81a73c941337ff
akaurh <arshxkaur@gmail.com>
2022-10-13 11:27:35 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTests {
+
+/* 
+	@Test 
+	public void testReverseInPlace() {
+    int[] input1 = { 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3 }, input1);
+	}
+
+
+  @Test
+  public void testReversed() {
+    int[] input1 = { };
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  } 
+
+  @Test
+  public void testInput()
+  {
+    int[] input1 = { 3, 4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3}, ArrayExamples.reversed(input1)); 
+
+  }
+}
+
+
+@Test
+public void testList()
+{
+
+}
+*/
+}
